ec2cb02cede70259469243ecd44463e881e93e13
Yuvraj Singh <ys508888@gmail.com>
2025-04-29 07:36:22 +0530

    backend fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://for04bluestacks:Unknown321@cluster0.9cewo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect(process.env.MONGODB_URL).then(() => console.log("DataBase Connected"));
 }
